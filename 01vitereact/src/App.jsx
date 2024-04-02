bf9551895dd7e503a908f916ab6be2e14df3dbf0
md0011 <mayurd0303@gmail.com>
2024-04-02 15:28:33 +0530

    React app with 'Vite'

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Hello from "./Hello";
+
+function App() {
+  const name = "Mayur Dev";
+  return (
+    <>
+      <Hello />
+      <h3>My name is {name}.</h3>
+      <p>Test Para</p>
+    </>
+  );
+}
+
+export default App;
